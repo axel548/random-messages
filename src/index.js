8f28e2da41ef4de61a1e6db017b155e07e4ecd5e
axel <axel.aatl@gmail.com>
2022-04-29 08:36:44 -0600

    Creación de un paquete para NPM

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const messages = [
+    "Oscar",
+    "Ana",
+    "Nicolay",
+    "Yessica",
+    "Diego",
+    "Laura",
+    "Carolina",
+    "Paulina"
+]
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+}
+
+module.exports = {
+    randomMsg
+}
